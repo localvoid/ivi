c260287d17fdec476f1474b76b22beb1f4107707
Boris Kaul <localvoid@gmail.com>
2017-03-12 08:18:44 +0700

    Improve selector display name in Dev Mode

--- src/state/selector.ts
+++ src/state/selector.ts
@@ -22,6 +22,15 @@ export function memoizeSelector<T, U extends SelectorData>(
     select: (prev: U | null, props: T, context: Context) => U,
     ref: (v?: U | null) => U | null,
 ): (prev: U | null, props: T, context: Context) => U {
+    if (__IVI_DEV__) {
+        const fn = function (prev: U | null, props: T, context: Context) {
+            const state = select(ref(), props, context);
+            ref(state);
+            return state;
+        };
+        fn.displayName = select.name;
+        return fn;
+    }
     return function (prev: U | null, props: T, context: Context) {
         const state = select(ref(), props, context);
         ref(state);
@@ -39,6 +48,13 @@ export function memoizeSelectorGlobally<T, U extends SelectorData>(
     select: (prev: U | null, props: T, context: Context) => U,
 ): (prev: U | null, props: T, context: Context) => U {
     let g: U | null = null;
+    if (__IVI_DEV__) {
+        const fn = function (prev: U | null, props: T, context: Context) {
+            return g = select(g, props, context);
+        };
+        fn.displayName = select.name;
+        return fn;
+    }
     return function (prev: U | null, props: T, context: Context) {
         return g = select(g, props, context);
     };
