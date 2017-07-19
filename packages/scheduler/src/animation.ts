1e50a84dd11e501becded11b85bf5c62a0f4674c
Boris Kaul <localvoid@gmail.com>
2017-07-19 08:33:06 +0700

    Animations should mark next frame as dirty even in sync mode

--- packages/scheduler/src/animation.ts
+++ packages/scheduler/src/animation.ts
@@ -1,5 +1,4 @@
 import { RepeatableTaskList, DEV_HOOKS, devModeAddHook } from "ivi-core";
-import { isSyncMode } from "./sync_mode";
 import { requestNextFrame } from "./frame";
 
 const _animations = new RepeatableTaskList();
@@ -11,11 +10,6 @@ const _animations = new RepeatableTaskList();
  */
 export function addAnimation(animation: () => boolean | undefined): void {
   _animations.add(animation);
-  if (__IVI_DEV__) {
-    if (isSyncMode()) {
-      return;
-    }
-  }
   requestNextFrame();
 }
 
@@ -27,11 +21,6 @@ export function executeAnimations(): void {
 }
 
 export function shouldRequestNextFrameForAnimations(): boolean {
-  if (__IVI_DEV__) {
-    if (isSyncMode()) {
-      return false;
-    }
-  }
   return (_animations.tasks.length > 0);
 }
 
