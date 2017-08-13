178516d06f507a2bf7f2c97af41e1e64fb000ff4
Boris Kaul <localvoid@gmail.com>
2017-08-13 12:36:01 +0700

    Add comments in ivi-test package

--- packages/ivi-test/src/dom.ts
+++ packages/ivi-test/src/dom.ts
@@ -5,6 +5,9 @@ import { VNodeWrapper } from "./vdom";
 
 let _container: HTMLDivElement | null = null;
 
+/**
+ * DOMRenderer is a helper object for testing Virtual DOM in a real DOM.
+ */
 export class DOMRenderer {
   private container: HTMLDivElement;
 
@@ -12,24 +15,46 @@ export class DOMRenderer {
     this.container = container;
   }
 
+  /**
+   * render renders a VNode in a test container and returns a VNodeWrapper object.
+   *
+   * @param vnode VNode.
+   * @returns VNodeWrapper object.
+   */
   render(vnode: VNode<any>): VNodeWrapper {
     render(vnode, this.container);
     return new VNodeWrapper(vnode, null, {});
   }
 
+  /**
+   * update triggers update in a scheduler.
+   */
   update(): void {
     update();
   }
 
+  /**
+   * nextTick triggers next tick in a scheduler and execute all microtasks, tasks and frame updates.
+   */
   nextTick(): void {
     triggerNextTick();
   }
 
+  /**
+   * nextFrame triggers next frame in a scheduler and executal all frame updates.
+   *
+   * @param time Current time.
+   */
   nextFrame(time?: number): void {
     triggerNextFrame(time);
   }
 }
 
+/**
+ * initDOMRenderer instantiates and initializes DOMRenderer object.
+ *
+ * @returns DOMRenderer.
+ */
 export function initDOMRenderer(): DOMRenderer {
   if (_container === null) {
     _container = document.createElement("div");
