6e62aaed6683096aac3d8a529f6db52be4417077
Boris Kaul <localvoid@gmail.com>
2017-08-27 12:14:10 +0700

    Add more modules with predefined vars

---
+++ packages/ivi-vars/electron-dev.js
@@ -0,0 +1,13 @@
+/**
+ * Development Mode.
+ * 
+ * @const {boolean}
+ */
+export const DEV = true;
+
+/**
+ * Target Environment.
+ * 
+ * @const {number}
+ */
+export const TARGET = 10;
---
+++ packages/ivi-vars/evergreen-browser.js
@@ -0,0 +1,13 @@
+/**
+ * Development Mode.
+ * 
+ * @const {boolean}
+ */
+export const DEV = false;
+
+/**
+ * Target Environment.
+ * 
+ * @const {number}
+ */
+export const TARGET = 6;
