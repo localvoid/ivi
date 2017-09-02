2db127f852442eba7d72b461d06d81139311d159
Boris Kaul <localvoid@gmail.com>
2017-09-02 12:35:37 +0700

    catchError should return fn() value

--- packages/ivi-core/src/error.ts
+++ packages/ivi-core/src/error.ts
@@ -4,11 +4,11 @@ export function addErrorHandler(handler: (e: any) => void): void {
   _errorHandlers.push(handler);
 }
 
-export function catchError(fn: (...args: any[]) => void): (...args: any[]) => void;
-export function catchError(fn: () => void): () => void {
+export function catchError(fn: (...args: any[]) => void): (...args: any[]) => any;
+export function catchError(fn: () => void): () => any {
   return function () {
     try {
-      fn.apply(null, arguments);
+      return fn.apply(null, arguments);
     } catch (e) {
       for (let i = 0; i < _errorHandlers.length; ++i) {
         _errorHandlers[i](e);
