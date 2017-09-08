2c75b945f06bf7a277cd85bc703a644ad0fd57da
Boris Kaul <localvoid@gmail.com>
2017-09-08 18:29:54 +0700

    Update rollup config for unpkg bundle

--- packages/ivi/rollup.conf.unpkg.js
+++ packages/ivi/rollup.conf.unpkg.js
@@ -1,18 +1,13 @@
 import resolve from "rollup-plugin-node-resolve";
-import replace from "rollup-plugin-replace";
 
 export default {
-  entry: __dirname + "/unpkg/entry.js",
-  dest: __dirname + "/dist/unpkg/index.js",
-  format: "umd",
-  moduleName: "ivi",
+  name: "ivi",
+  input: __dirname + "/unpkg/entry.js",
+  output: {
+    file: __dirname + "/dist/unpkg/index.js",
+    format: "umd",
+  },
   plugins: [
     resolve(),
-    replace({
-      values: {
-        __IVI_DEV__: true,
-        __IVI_BROWSER__: true,
-      },
-    }),
   ],
 };
