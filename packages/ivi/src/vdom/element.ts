b9c6aaf32bae8c89c28dfc31e74c589e00ee4641
Boris Kaul <localvoid@gmail.com>
2018-04-26 20:33:16 +0700

    Cache flags in element factory

--- packages/ivi/src/vdom/element.ts
+++ packages/ivi/src/vdom/element.ts
@@ -2,9 +2,10 @@ import { VNode } from "./vnode";
 import { VNodeFlags } from "..";
 
 export function elementFactory<P, N>(element: VNode<P, N>): (className?: string) => VNode<P, N> {
+  const flags = element._flags | VNodeFlags.ElementFactory;
   return function (className?: string) {
     return new VNode<P, N>(
-      element._flags | VNodeFlags.ElementFactory,
+      flags,
       element,
       null,
       className === undefined ? null : className,
