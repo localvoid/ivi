3327d5a49c0555c5cc1f627aca786e2b9fa74fc6
Boris Kaul <localvoid@gmail.com>
2018-05-06 13:30:36 +0700

    Refactor children tests

--- packages/ivi/__tests__/render_children.spec.ts
+++ packages/ivi/__tests__/render_children.spec.ts
@@ -3,20 +3,19 @@ import * as h from "./utils/html";
 
 test(`<div>{ null }</div>`, () => {
   checkDOMOps((c) => {
-    const n = render<HTMLElement>(h.div().c(null));
+    const n = render(h.div().c(null));
 
-    expect(n.childNodes.length).toBe(0);
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(1, 0, 0, 0, 1, 0, 0));
   });
 });
 
-test(`<div>abc</div>`, () => {
+test(`<div>"abc"</div>`, () => {
   checkDOMOps((c) => {
     const v = h.div().c("abc");
-    const n = render<HTMLElement>(v);
+    const n = render(v);
 
-    expect(n.childNodes.length).toBe(1);
-    expect(n.firstChild!.nodeValue).toBe("abc");
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
   });
 });
@@ -24,10 +23,9 @@ test(`<div>abc</div>`, () => {
 test(`<div>10</div>`, () => {
   checkDOMOps((c) => {
     const v = h.div().c(10);
-    const n = render<HTMLElement>(v);
+    const n = render(v);
 
-    expect(n.childNodes.length).toBe(1);
-    expect(n.firstChild!.nodeValue).toBe("10");
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
   });
 });
@@ -39,10 +37,9 @@ test(`<div><span></span></div>`, () => {
         h.span(),
       )
     );
-    const n = render<HTMLElement>(v);
+    const n = render(v);
 
-    expect(n.childNodes.length).toBe(1);
-    expect(n.children[0].tagName.toLowerCase()).toBe("span");
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(2, 0, 0, 0, 2, 0, 0));
   });
 });
@@ -55,11 +52,9 @@ test(`<div><span></span><strong></strong></div>`, () => {
         h.strong(),
       )
     );
-    const n = render<HTMLElement>(v);
+    const n = render(v);
 
-    expect(n.childNodes.length).toBe(2);
-    expect(n.children[0].tagName.toLowerCase()).toBe("span");
-    expect(n.children[1].tagName.toLowerCase()).toBe("strong");
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(3, 0, 0, 0, 3, 0, 0));
   });
 });
@@ -73,16 +68,14 @@ test(`<div><div></div>{ null }<span></span></div>`, () => {
         h.span(),
       )
     );
-    const n = render<HTMLElement>(v);
+    const n = render(v);
 
-    expect(n.childNodes.length).toBe(2);
-    expect(n.children[0].tagName.toLowerCase()).toBe("div");
-    expect(n.children[1].tagName.toLowerCase()).toBe("span");
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(3, 0, 0, 0, 3, 0, 0));
   });
 });
 
-test(`<div><div></div>abc<span></span></div>`, () => {
+test(`<div><div></div>"abc"<span></span></div>`, () => {
   checkDOMOps((c) => {
     const v = (
       h.div().c(
@@ -91,12 +84,9 @@ test(`<div><div></div>abc<span></span></div>`, () => {
         h.span(),
       )
     );
-    const n = render<HTMLElement>(v);
+    const n = render(v);
 
-    expect(n.childNodes.length).toBe(3);
-    expect(n.children[0].tagName.toLowerCase()).toBe("div");
-    expect(n.childNodes[1].nodeValue).toBe("abc");
-    expect(n.children[1].tagName.toLowerCase()).toBe("span");
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(3, 0, 1, 0, 4, 0, 0));
   });
 });
@@ -110,12 +100,9 @@ test(`<div><div></div>123<span></span></div>`, () => {
         h.span(),
       )
     );
-    const n = render<HTMLElement>(v);
+    const n = render(v);
 
-    expect(n.childNodes.length).toBe(3);
-    expect(n.children[0].tagName.toLowerCase()).toBe("div");
-    expect(n.childNodes[1].nodeValue).toBe("123");
-    expect(n.children[1].tagName.toLowerCase()).toBe("span");
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(3, 0, 1, 0, 4, 0, 0));
   });
 });
@@ -135,28 +122,9 @@ test(`complex tree #1`, () => {
         h.div(),
       )
     );
-    const n = render<HTMLElement>(v);
-    expect(n.childNodes.length).toBe(4);
-    expect(n.children[0].tagName.toLowerCase()).toBe("div");
-    expect(n.children[1].tagName.toLowerCase()).toBe("div");
-    expect(n.children[2].tagName.toLowerCase()).toBe("div");
-    expect(n.children[3].tagName.toLowerCase()).toBe("div");
+    const n = render(v);
 
-    expect(n.children[0].childNodes.length).toBe(1);
-    expect(n.children[1].childNodes.length).toBe(2);
-    expect(n.children[2].childNodes.length).toBe(1);
-    expect(n.children[3].childNodes.length).toBe(0);
-
-    expect(n.children[0].firstChild!.nodeValue).toBe("hello");
-
-    expect(n.children[1].children[0].tagName.toLowerCase()).toBe("span");
-    expect(n.children[1].children[0].firstChild!.nodeValue).toBe("world");
-    expect(n.children[1].children[1].tagName.toLowerCase()).toBe("div");
-    expect(n.children[1].children[1].childNodes.length).toBe(1);
-    expect(n.children[1].children[1].children[0].tagName.toLowerCase()).toBe("span");
-
-    expect(n.children[2].children[0].tagName.toLowerCase()).toBe("div");
-
+    expect(n).toMatchSnapshot();
     expect(c).toEqual(domOps(9, 0, 2, 0, 11, 0, 0));
   });
 });
---
+++ packages/ivi/__tests__/sync_children_basic.spec.ts
@@ -0,0 +1,194 @@
+import { startRender, checkDOMOps, domOps } from "./utils";
+import * as h from "./utils/html";
+
+test(`<div></div> => <div>"abc"</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div());
+      const n = r(h.div().c("abc"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div></div> => <div>10</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div());
+      const n = r(h.div().c(10));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>"abc"</div> => <div></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c("abc"));
+      const n = r(h.div());
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>10</div> => <div></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c(10));
+      const n = r(h.div());
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>"abc"</div> => <div>"abc"</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c("abc"));
+      const n = r(h.div().c("abc"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>10</div> => <div>10</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c(10));
+      const n = r(h.div().c(10));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>"abc"</div> => <div>"cde"</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c("abc"));
+      const n = r(h.div().c("cde"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>""</div> => <div>"cde"</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c(""));
+      const n = r(h.div().c("cde"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>"abc"</div> => <div>10</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c("abc"));
+      const n = r(h.div().c(10));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>10</div> => <div>"abc"</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c(10));
+      const n = r(h.div().c("abc"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 1, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div>{ null }</div> => <div><div></div></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div());
+      const n = r(h.div().c(h.div())) as HTMLElement;
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(2, 0, 0, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div><div></div></div> => <div>{ null }</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c(h.div()));
+      const n = r(h.div().c(null));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(2, 0, 0, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div><div></div> => <div>"cde"</div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c(h.div()));
+      const n = r(h.div().c("cde"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(2, 0, 1, 0, 2, 1, 0));
+    });
+  });
+});
+
+test(`<div>"cde"</div> => <div><div></div></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c("cde"));
+      const n = r(h.div().c(h.div()));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(2, 0, 1, 0, 2, 1, 0));
+    });
+  });
+});
+
+test(`<div></div> => <div><div></div></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div());
+      const n = r(h.div().c(h.div()));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(2, 0, 0, 0, 2, 0, 0));
+    });
+  });
+});
+
+test(`<div><div></div></div> => <div></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().c(h.div()));
+      const n = r(h.div());
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(2, 0, 0, 0, 2, 0, 0));
+    });
+  });
+});
