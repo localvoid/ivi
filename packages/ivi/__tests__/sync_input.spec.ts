1f16c8f25af339447360f52c5bba5301c152118d
Boris Kaul <localvoid@gmail.com>
2018-05-06 19:25:39 +0700

    Refactor sync input tests

---
+++ packages/ivi/__tests__/sync_input.spec.ts
@@ -0,0 +1,86 @@
+import { startRender, checkDOMOps, domOps } from "./utils";
+import * as h from "./utils/html";
+
+test(`<input> => <input type="checkbox">`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.input());
+      const n = r(h.inputCheckbox());
+
+      expect(n).toMatchSnapshot();
+      expect(c).toMatchSnapshot();
+    });
+  });
+});
+
+test(`<input> => <input value="cde">`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.input());
+      const n = r(h.input().value("cde"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toMatchSnapshot();
+    });
+  });
+});
+
+test(`<input value="abc"> => <input value="cde">`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.input().value("abc"));
+      const n = r(h.input().value("cde"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toMatchSnapshot();
+    });
+  });
+});
+
+test(`<input value="abc"> => <input>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.input().value("abc"));
+      const n = r(h.input());
+
+      expect(n).toMatchSnapshot();
+      expect(c).toMatchSnapshot();
+    });
+  });
+});
+
+test(`<input type="checkbox"> => <input type="checkbox" checked=true>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.inputCheckbox());
+      const n = r(h.inputCheckbox().checked(true));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toMatchSnapshot();
+    });
+  });
+});
+
+test(`<input type="checkbox" checked=true> => <input type="checkbox" checked=false>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.inputCheckbox().checked(true));
+      const n = r(h.inputCheckbox().checked(false));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toMatchSnapshot();
+    });
+  });
+});
+
+test(`<input type="checkbox" checked=true> => <input type="checkbox">`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.inputCheckbox().checked(true));
+      const n = r(h.inputCheckbox());
+
+      expect(n).toMatchSnapshot();
+      expect(c).toMatchSnapshot();
+    });
+  });
+});
