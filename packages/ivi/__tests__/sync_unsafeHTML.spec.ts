5f757f7c642743c8ff517a002e961e2c23ea21c2
Boris Kaul <localvoid@gmail.com>
2018-05-07 11:28:51 +0700

    Add sync unsafeHTML tests

--- packages/ivi/__tests__/sync_unsafeHTML.spec.ts
+++ packages/ivi/__tests__/sync_unsafeHTML.spec.ts
@@ -12,3 +12,51 @@ test(`<div> => <div unsafeHTML="<span>abc</span>"></div>`, () => {
     });
   });
 });
+
+test(`<div unsafeHTML="<div>abc</div>"> => <div></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().unsafeHTML("<div>abc</div>"));
+      const n = r(h.div());
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(2, 0, 0, 0, 1, 1, 0));
+    });
+  });
+});
+
+test(`<div unsafeHTML={ null }> => <div unsafeHTML="<span>abc</span>"></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().unsafeHTML(null));
+      const n = r(h.div().unsafeHTML("<span>abc</span>"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 0, 0, 1, 0, 0));
+    });
+  });
+});
+
+test(`<div unsafeHTML="<div>abc</div>"> => <div unsafeHTML={ null }></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().unsafeHTML("<div>abc</div>"));
+      const n = r(h.div().unsafeHTML(null));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 0, 0, 1, 0, 0));
+    });
+  });
+});
+
+test(`<div unsafeHTML="<div>abc</div>"> => <div unsafeHTML="<span>abc</span>"></div>`, () => {
+  startRender((r) => {
+    checkDOMOps((c) => {
+      r(h.div().unsafeHTML("<div>abc</div>"));
+      const n = r(h.div().unsafeHTML("<span>abc</span>"));
+
+      expect(n).toMatchSnapshot();
+      expect(c).toEqual(domOps(1, 0, 0, 0, 1, 0, 0));
+    });
+  });
+});
