b834e5c6730882f21d64e60b209d174a9f214a7d
Boris Kaul <localvoid@gmail.com>
2018-05-07 11:23:24 +0700

    Add SVG className tests

--- packages/ivi/__tests__/sync_className.spec.ts
+++ packages/ivi/__tests__/sync_className.spec.ts
@@ -1,74 +1,108 @@
 import { startRender } from "./utils";
 import * as h from "./utils/html";
+import * as s from "./utils/svg";
 
-describe(`sync element className`, () => {
-  test(`null => "a"`, () => {
-    startRender<HTMLElement>((r) => {
-      r(h.div());
-      const b = r(h.div("a"));
+describe("HTML", () => {
+  describe(`sync className`, () => {
+    test(`null => "a"`, () => {
+      startRender<HTMLElement>((r) => {
+        r(h.div());
+        const b = r(h.div("a"));
 
-      expect(b.classList.length).toBe(1);
-      expect(b.classList.contains("a")).toBe(true);
+        expect(b.classList.length).toBe(1);
+        expect(b.classList.contains("a")).toBe(true);
+      });
+    });
+
+    test(`"a" => null`, () => {
+      startRender<HTMLElement>((r) => {
+        r(h.div("a"));
+        const b = r(h.div());
+
+        expect(b.classList.length).toBe(0);
+      });
+    });
+
+    test(`"a" => "a"`, () => {
+      startRender<HTMLElement>((r) => {
+        r(h.div("a"));
+        const b = r(h.div("a"));
+
+        expect(b.classList.length).toBe(1);
+        expect(b.classList.contains("a")).toBe(true);
+      });
+    });
+
+    test(`"a b" => "a"`, () => {
+      startRender<HTMLElement>((r) => {
+        r(h.div("a"));
+        const b = r(h.div("a"));
+
+        expect(b.classList.length).toBe(1);
+        expect(b.classList.contains("a")).toBe(true);
+      });
+    });
+
+    test(`"a" => "a b"`, () => {
+      startRender<HTMLElement>((r) => {
+        r(h.div("a"));
+        const b = r(h.div("a b"));
+
+        expect(b.classList.length).toBe(2);
+        expect(b.classList.contains("a")).toBe(true);
+        expect(b.classList.contains("b")).toBe(true);
+      });
+    });
+
+    test(`null => "a b"`, () => {
+      startRender<HTMLElement>((r) => {
+        r(h.div());
+        const b = r(h.div("a b"));
+
+        expect(b.classList.length).toBe(2);
+        expect(b.classList.contains("a")).toBe(true);
+        expect(b.classList.contains("b")).toBe(true);
+      });
+    });
+
+    test(`"a b" => null`, () => {
+      startRender<HTMLElement>((r) => {
+        r(h.div("a b"));
+        const b = r(h.div());
+
+        expect(b.classList.length).toBe(0);
+      });
     });
   });
+});
 
-  test(`"a" => null`, () => {
-    startRender<HTMLElement>((r) => {
-      r(h.div("a"));
-      const b = r(h.div());
+describe("SVG", () => {
+  describe(`sync className`, () => {
+    test(`null => "a"`, () => {
+      startRender<SVGElement>((r) => {
+        r(s.circle());
+        const b = r(s.circle("a"));
 
-      expect(b.classList.length).toBe(0);
+        expect(b.getAttribute("class")).toBe("a");
+      });
     });
-  });
 
-  test(`"a" => "a"`, () => {
-    startRender<HTMLElement>((r) => {
-      r(h.div("a"));
-      const b = r(h.div("a"));
+    test(`"a" => null`, () => {
+      startRender<SVGElement>((r) => {
+        r(s.circle("a"));
+        const b = r(s.circle());
 
-      expect(b.classList.length).toBe(1);
-      expect(b.classList.contains("a")).toBe(true);
+        expect(b.getAttribute("class")).toBe("");
+      });
     });
-  });
 
-  test(`"a b" => "a"`, () => {
-    startRender<HTMLElement>((r) => {
-      r(h.div("a"));
-      const b = r(h.div("a"));
+    test(`"a" => "a"`, () => {
+      startRender<SVGElement>((r) => {
+        r(s.circle("a"));
+        const b = r(s.circle("a"));
 
-      expect(b.classList.length).toBe(1);
-      expect(b.classList.contains("a")).toBe(true);
-    });
-  });
-
-  test(`"a" => "a b"`, () => {
-    startRender<HTMLElement>((r) => {
-      r(h.div("a"));
-      const b = r(h.div("a b"));
-
-      expect(b.classList.length).toBe(2);
-      expect(b.classList.contains("a")).toBe(true);
-      expect(b.classList.contains("b")).toBe(true);
-    });
-  });
-
-  test(`null => "a b"`, () => {
-    startRender<HTMLElement>((r) => {
-      r(h.div());
-      const b = r(h.div("a b"));
-
-      expect(b.classList.length).toBe(2);
-      expect(b.classList.contains("a")).toBe(true);
-      expect(b.classList.contains("b")).toBe(true);
-    });
-  });
-
-  test(`"a b" => null`, () => {
-    startRender<HTMLElement>((r) => {
-      r(h.div("a b"));
-      const b = r(h.div());
-
-      expect(b.classList.length).toBe(0);
+        expect(b.getAttribute("class")).toBe("a");
+      });
     });
   });
 });
