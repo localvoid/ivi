0f91dec2bf5daecce65cb16ba95189061ef4ce31
Boris Kaul <localvoid@gmail.com>
2018-05-08 07:57:07 +0700

    Add more element factory tests

---
+++ packages/ivi/__tests__/element_factory.spec.ts
@@ -0,0 +1,36 @@
+import { VNodeFlags } from "../src/vdom/flags";
+import { elementFactory } from "../src/vdom/element";
+import * as h from "./utils/html";
+
+const div = h.div();
+const input = h.input();
+const divFactory = elementFactory(div);
+const inputFactory = elementFactory(input);
+
+test(`div flags`, () => {
+  expect(divFactory()._flags & ~VNodeFlags.ElementFactory).toBe(h.div()._flags);
+});
+
+test(`input flags`, () => {
+  expect(inputFactory()._flags & ~VNodeFlags.ElementFactory).toBe(h.input()._flags);
+});
+
+test(`div factory`, () => {
+  expect(divFactory()._tag).toBe(div);
+});
+
+test(`input factory`, () => {
+  expect(inputFactory()._tag).toBe(input);
+});
+
+test(`default className = undefined`, () => {
+  expect(divFactory()._className).toBeUndefined();
+});
+
+test(`className = undefined`, () => {
+  expect(divFactory(undefined)._className).toBeUndefined();
+});
+
+test(`className = "a"`, () => {
+  expect(divFactory("a")._className).toBe("a");
+});
