a62a2ba223d296dc13af73d9bbeda9c450916422
Boris Kaul <localvoid@gmail.com>
2018-05-10 23:21:13 +0700

    test(core): add global error handler tests

---
+++ packages/ivi-core/__tests__/error_handler.spec.ts
@@ -0,0 +1,34 @@
+import { addErrorHandler, catchError } from "../src/error";
+
+const handler = jest.fn();
+addErrorHandler(handler);
+
+test(`handler should not be invoked when it is added`, () => {
+  expect(handler.mock.calls.length).toBe(0);
+});
+
+describe(`catchError`, () => {
+  beforeEach(() => {
+    handler.mockReset();
+  });
+
+  test(`handler should be invoked with an error raised from inside`, () => {
+    const e = new Error();
+    try {
+      catchError(() => { throw e; })();
+    } catch (e) {
+      //
+    }
+
+    expect(handler.mock.calls.length).toBe(1);
+    expect(handler.mock.calls[0][0]).toBe(e);
+  });
+
+  test(`arguments should be passed to the wrapped function`, () => {
+    const fn = jest.fn();
+    catchError(fn)(1, 2);
+
+    expect(fn.mock.calls[0][0]).toBe(1);
+    expect(fn.mock.calls[0][1]).toBe(2);
+  });
+});
