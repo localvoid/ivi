03c46de99da4cd73437e903ef54d498a94c5a834
Boris Kaul <localvoid@gmail.com>
2018-11-08 14:47:59 +0700

    fix(typings): add function overloading to selector() function

--- packages/ivi/src/vdom/utils.ts
+++ packages/ivi/src/vdom/utils.ts
@@ -21,7 +21,83 @@ import { useSelect } from "./hooks";
  * @param shouldUpdate - Should update function.
  * @returns selector factory.
  */
-export const selector = <T, P, C>(
+export function selector<T>(
+  s: () => T,
+): (c: Component) => () => T;
+
+/**
+ * selector creates a selector factory.
+ *
+ * @example
+ *
+ *     interface Entry {
+ *       title: string;
+ *     }
+ *     const useEntryTitle = selector((entry: Entry) => entry.title);
+ *
+ *     const EntryView = component<Entry>((c) => {
+ *       const getEntryTitle = useEntryTitle(c);
+ *
+ *       return (entry) => div().t(getEntryTitle(entry));
+ *     });
+ *
+ * @param s - Selector function.
+ * @param shouldUpdate - Should update function.
+ * @returns selector factory.
+ */
+export function selector<T, P>(
+  s: (props: P, context: undefined, prev?: T | undefined) => T,
+  shouldUpdate?: undefined extends P ? undefined : (prev: P, next: P) => boolean,
+): (c: Component) => undefined extends P ? () => T : (props: P) => T;
+
+/**
+ * selector creates a selector factory.
+ *
+ * @example
+ *
+ *     interface Entry {
+ *       title: string;
+ *     }
+ *     const useEntryTitle = selector((entry: Entry) => entry.title);
+ *
+ *     const EntryView = component<Entry>((c) => {
+ *       const getEntryTitle = useEntryTitle(c);
+ *
+ *       return (entry) => div().t(getEntryTitle(entry));
+ *     });
+ *
+ * @param s - Selector function.
+ * @param shouldUpdate - Should update function.
+ * @returns selector factory.
+ */
+export function selector<T, P, C>(
   s: (props: P, context: C, prev?: T | undefined) => T,
   shouldUpdate?: undefined extends P ? undefined : (prev: P, next: P) => boolean,
-) => (c: Component) => useSelect(c, s, shouldUpdate);
+): (c: Component) => undefined extends P ? () => T : (props: P) => T;
+
+/**
+ * selector creates a selector factory.
+ *
+ * @example
+ *
+ *     interface Entry {
+ *       title: string;
+ *     }
+ *     const useEntryTitle = selector((entry: Entry) => entry.title);
+ *
+ *     const EntryView = component<Entry>((c) => {
+ *       const getEntryTitle = useEntryTitle(c);
+ *
+ *       return (entry) => div().t(getEntryTitle(entry));
+ *     });
+ *
+ * @param s - Selector function.
+ * @param shouldUpdate - Should update function.
+ * @returns selector factory.
+ */
+export function selector<T, P, C>(
+  s: (props: P, context: C, prev?: T | undefined) => T,
+  shouldUpdate?: undefined extends P ? undefined : (prev: P, next: P) => boolean,
+): (c: Component) => undefined extends P ? () => T : (props: P) => T {
+  return (c: Component) => useSelect(c, s, shouldUpdate);
+}
