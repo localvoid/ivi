dd2381b9e238fc488f142e1cd5924de1f4c4848c
Boris Kaul <localvoid@gmail.com>
2018-11-22 18:45:20 +0700

    docs(vdom): add comments

--- packages/ivi/src/vdom/operations.ts
+++ packages/ivi/src/vdom/operations.ts
@@ -5,46 +5,148 @@ import { StateNode } from "./state";
 import { ElementProtoDescriptor } from "./element_proto";
 import { ComponentDescriptor } from "./component";
 
+/**
+ * Operation type.
+ */
 export interface OpType {
+  /**
+   * See {@link NodeFlags} for details.
+   */
   readonly flags: NodeFlags;
+  /**
+   * Operation type descriptor.
+   */
   readonly descriptor: ComponentDescriptor | ElementProtoDescriptor | string | null;
 }
 
+/**
+ * createOpType creates {@link OpType} instances.
+ *
+ * @param flags See {@link NodeFlags} for details.
+ * @param descriptor Operation type descriptor.
+ * @returns {@link OpType} instance.
+ */
 export const createOpType = (
   flags: NodeFlags,
   descriptor: ComponentDescriptor | ElementProtoDescriptor | string | null,
 ): OpType => ({ flags, descriptor });
 
+/**
+ * OpType for Events nodes.
+ */
 export const EVENTS = createOpType(NodeFlags.Events, null);
+
+/**
+ * OpType for Ref nodes.
+ */
 export const REF = createOpType(NodeFlags.Ref, null);
+
+/**
+ * OpType for Context nodes.
+ */
 export const CONTEXT = createOpType(NodeFlags.Context, null);
+
+/**
+ * OpType for TrackByKey nodes.
+ */
 export const TRACK_BY_KEY = createOpType(NodeFlags.TrackByKey | NodeFlags.MultipleChildren, null);
 
+/**
+ * Operation node.
+ */
 export interface OpNode<T = any> {
+  /**
+   * Operation type.
+   */
   readonly type: OpType;
+  /**
+   * Operation data.
+   */
   readonly data: T;
 }
 
+/**
+ * createOpNode creates an {@link OpNode} instance.
+ *
+ * @param type Operation type.
+ * @param data Operation data.
+ * @returns {@link OpNode} instance.
+ */
 export const createOpNode = <T>(type: OpType, data: T): OpNode<T> => ({ type, data });
 
+/**
+ * Operation data for element operations.
+ */
 export interface ElementData<T = any> {
+  /**
+   * Element class name.
+   */
   readonly className: string | undefined;
+  /**
+   * Element attributes.
+   */
   readonly attrs: T | undefined;
+  /**
+   * Children operations.
+   */
   readonly children: OpChildren;
 }
 
+/**
+ * Children operations supported by element nodes.
+ */
 export type OpChildren = string | number | OpNode | RecursiveOpChildrenArray | null;
+
+/**
+ * Recursive children array operations supported by element nodes.
+ */
 export interface RecursiveOpChildrenArray extends Array<OpChildren> { }
 
+/**
+ * Generic operation data for simple operations.
+ */
 export interface OpData<T = any> {
+  /**
+   * Generic data.
+   */
   readonly data: T;
+  /**
+   * Child operation.
+   */
   readonly child: OpNode | string | number | null;
 }
 
+/**
+ * Operation data for Events operations.
+ */
 export type EventsData = OpData<EventHandler | Array<EventHandler | null> | null>;
+
+/**
+ * Operation data for Ref operations.
+ */
 export type RefData = OpData<Box<StateNode>>;
+
+/**
+ * Operation data for Context operations.
+ */
 export type ContextData = OpData<{}>;
 
+/**
+ * Operation factory for event handlers.
+ *
+ * @example
+ *
+ *     render(
+ *       Events(onClick(() => { console.log("clicked"); }),
+ *         ChildComponent(),
+ *       ),
+ *       DOMContainer,
+ *     );
+ *
+ * @param data Event handlers.
+ * @param child Child operation.
+ * @returns Event handler OpNode.
+ */
 export const Events = DEBUG ?
   (
     data: EventHandler | Array<EventHandler | null> | null,
@@ -62,6 +164,26 @@ export const Events = DEBUG ?
       createOpNode(EVENTS, { data, child })
     );
 
+/**
+ * Operation factory for ref nodes.
+ *
+ * @example
+ *
+ *     const _ref = box();
+ *
+ *     render(
+ *       Ref(_ref,
+ *         div(),
+ *       ),
+ *       DOMContainer,
+ *     );
+ *
+ *     getDOMNode(_ref);
+ *
+ * @param data Boxed value.
+ * @param child Child operation.
+ * @returns Ref OpNode.
+ */
 export const Ref = DEBUG ?
   (
     data: Box<StateNode>,
@@ -78,7 +200,7 @@ export const Ref = DEBUG ?
   ): OpNode<RefData> => createOpNode(REF, { data, child });
 
 /**
- * Context creates a context OpNode that will modify current context.
+ * Operation factory for context nodes.
  *
  * @example
  *
@@ -89,9 +211,9 @@ export const Ref = DEBUG ?
  *       DOMContainer,
  *     );
  *
- * @param ctx - Context object
- * @param child - child OpNode
- * @returns context OpNode
+ * @param data Context object.
+ * @param child Child operation.
+ * @returns Context OpNode.
  */
 export const Context = DEBUG ?
   (data: {}, child: OpNode | string | number | null): OpNode<ContextData> => {
@@ -102,12 +224,43 @@ export const Context = DEBUG ?
   } :
   (data: {}, child: OpNode | string | number | null): OpNode<ContextData> => createOpNode(CONTEXT, { data, child });
 
+/**
+ * Key is an object that is used by TrackByKey operations to track operations.
+ */
 export interface Key<K, V> {
+  /**
+   * Key.
+   */
   readonly k: K;
+  /**
+   * Value.
+   */
   readonly v: V;
 }
+
+/**
+ * key creates a {@link Key} instance.
+ *
+ * @param k Key.
+ * @param v Value.
+ * @returns {@link Key} instance.
+ */
 export const key = <K, V>(k: K, v: V): Key<K, V> => ({ k, v });
 
+/**
+ * Operation factory for track by key nodes.
+ *
+ * @example
+ *
+ *     const items = [1, 2, 3];
+ *     render(
+ *       TrackByKey(items.map((item) => key(item, div(_, _, item))),
+ *       DOMContainer,
+ *     );
+ *
+ * @param items Keyed operations.
+ * @returns Track by key OpNode.
+ */
 export const TrackByKey = DEBUG ?
   <T>(items: Key<T, OpNode | number | string>[]) => {
     if (DEBUG) {
