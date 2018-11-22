37cd8576e4e6544beefae87b4f7319f7e698c5c5
Boris Kaul <localvoid@gmail.com>
2018-11-22 13:27:13 +0700

    perf(vdom): stateless components shouldn't be dirty checked

--- packages/ivi/src/vdom/factories.ts
+++ packages/ivi/src/vdom/factories.ts
@@ -184,7 +184,8 @@ export function statelessComponent<P>(
   update: (props: P) => OpNode,
   shouldUpdate?: undefined extends P ? undefined : (prev: P, next: P) => boolean,
 ): (props: P) => OpNode<P> {
-  return component(() => update, shouldUpdate);
+  const type = createOpType(NodeFlags.Component, { c: () => update, shouldUpdate });
+  return (props: P) => createOpNode(type, props);
 }
 
 /**
