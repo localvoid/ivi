83dc07aa88f666dddac3d34ce0e634b99296d73a
Boris Kaul <localvoid@gmail.com>
2018-12-04 12:50:50 +0700

    feat(vdom): Refs should reset previous value to null

--- packages/ivi/src/vdom/ref.ts
+++ packages/ivi/src/vdom/ref.ts
@@ -4,11 +4,25 @@ import { OpState } from "./state";
 import { component } from "./factories";
 import { useUnmount } from "./hooks";
 
-const _Ref = component<{ ref: Box<OpState | null>, child: Op }>((c) => {
+function updateRef(prev: Box<OpState | null> | undefined, next: Box<OpState | null>, s: OpState): Box<OpState | null> {
+  if (prev !== void 0 && prev !== next) {
+    prev.v = null;
+  }
+  next.v = s;
+  return next;
+}
+
+const _Ref = component<{ r: Box<OpState | null>, c: Op }>((s) => {
   let _ref: Box<OpState | null>;
-  useUnmount(c, () => {
-    _ref.v = null;
-  });
-  return ({ ref, child }) => (_ref = ref, ref.v = c, child);
+  useUnmount(s, () => { _ref.v = null; });
+  return ({ r, c }) => (_ref = updateRef(_ref, r, s), c);
 });
-export const Ref = (ref: Box<OpState | null>, child: Op) => _Ref({ ref, child });
+
+/**
+ * Ref creates a component that captures internal state into a boxed value.
+ *
+ * @param r Boxed value.
+ * @param c Children operations.
+ * @returns Ref node.
+ */
+export const Ref = (r: Box<OpState | null>, c: Op) => _Ref({ r, c });
