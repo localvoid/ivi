e097c53ccdd32c26d2e23f3aed675f1eb86f7127
Boris Kaul <localvoid@gmail.com>
2019-02-06 22:08:46 +0700

    refactor(ssr): codegolf

--- packages/ivi/src/ssr/render.ts
+++ packages/ivi/src/ssr/render.ts
@@ -1,35 +1,38 @@
 import { NodeFlags } from "../vdom/node_flags";
 import { AttributeDirective } from "../vdom/attribute_directive";
-import { OpNode, Op, OpArray, ElementData, ContextData, Key } from "../vdom/operations";
+import { OpNode, Op, ElementData, ContextData, Key } from "../vdom/operations";
 import { ComponentDescriptor, StatelessComponentDescriptor } from "../vdom/component";
 import { ElementProtoDescriptor } from "../vdom/element_proto";
 import { createStateNode } from "../vdom/state";
 import { setContext, restoreContext } from "../vdom/context";
 import { escapeAttributeValue, escapeText } from "./escape";
 
-function renderText(text: string | number): string {
-  if (typeof text === "string") {
-    return escapeText(text);
-  }
-  return text.toString();
-}
-
-function renderFragment(children: OpArray): string {
-  let result = "";
-  for (let i = 0; i < children.length; ++i) {
-    result += renderToString(children[i]);
-  }
-  return result;
-}
-
 let _attributes = "";
 let _styles = "";
 let _children = "";
 
+/**
+ * emitAttribute pushes attribute string.
+ *
+ * @example
+ *
+ *   emitAttribute(`id="abc"`);
+ *
+ * @param attr Attribute string.
+ */
 export function emitAttribute(attr: string): void {
   _attributes += ` ${attr}`;
 }
 
+/**
+ * emitStyle pushes style string.
+ *
+ * @example
+ *
+ *   emitStyle(`padding:0`);
+ *
+ * @param style Style string.
+ */
 export function emitStyle(style: string): void {
   if (_styles !== "") {
     _styles += ";";
@@ -37,94 +40,17 @@ export function emitStyle(style: string): void {
   _styles += style;
 }
 
+/**
+ * emitChildrens pushes children string.
+ *
+ * @example
+ *
+ *   emitChildren(`<h1>Hello World</h1>`);
+ *
+ * @param children Children string.
+ */
 export function emitChildren(children: string | number): void {
   _children += children;
-}
-
-function renderNode(op: OpNode): string {
-  const flags = op.t.f;
-  let result;
-  if ((flags & NodeFlags.Element) !== 0) {
-    const data = op.d;
-    let openElement = `<${op.t.d}`;
-    let children = "";
-
-    const className = data.n;
-    if (className !== void 0 && className !== "") {
-      openElement += ` class="${className}"`;
-    }
-    let attrs = data.a;
-    if ((op.t.f & NodeFlags.ElementProto) !== 0 && (op.t.d as ElementProtoDescriptor).p.d.a !== void 0) {
-      attrs = { ...(op.t.d as ElementProtoDescriptor).p.d.a, ...attrs };
-    }
-    if (attrs !== void 0) {
-      for (const key in attrs) {
-        const value = attrs[key];
-        if (key === "style") {
-          for (const skey in value) {
-            emitStyle(`${skey}:${escapeAttributeValue(value[skey])}`);
-          }
-        } else {
-          if (typeof value === "object") {
-            (value as AttributeDirective<any>).s!(key, (value as AttributeDirective<any>).v);
-          } else if (typeof value !== "boolean") {
-            if (value !== null) {
-              emitAttribute(`${key}="${escapeAttributeValue(value)}"`);
-            }
-          } else if (value === true) {
-            emitAttribute(key);
-          }
-        }
-      }
-      if (_styles !== "") {
-        emitAttribute(`style="${_styles}"`);
-        _styles = "";
-      }
-      openElement += _attributes;
-      children = _children;
-      _attributes = "";
-      _children = "";
-    }
-
-    if (children === "") {
-      children = renderToString((op as OpNode<ElementData>).d.c);
-    }
-    if ((flags & NodeFlags.NewlineEatingElement) !== 0) {
-      if (children.length > 0 && children.charCodeAt(0) === 10) { // "\n"
-        children = `\n${children}`;
-      }
-    }
-    return ((flags & (NodeFlags.VoidElement | NodeFlags.Svg)) !== 0 && children === "") ?
-      `${openElement} />` :
-      `${openElement}>${children}</${op.t.d}>`;
-  }
-  if ((flags & NodeFlags.Component) !== 0) {
-    if ((flags & NodeFlags.Stateful) !== 0) {
-      const stateNode = createStateNode(op);
-      stateNode.s = { r: null, s: null, u: null };
-      return renderToString((op.t.d as ComponentDescriptor).c(stateNode)(op.d));
-    } else {
-      return renderToString((op.t.d as StatelessComponentDescriptor).c(op.d));
-    }
-  }
-  if ((flags & (NodeFlags.Events | NodeFlags.Context)) !== 0) {
-    if ((flags & NodeFlags.Context) !== 0) {
-      const contextData = (op.d as ContextData);
-      const prevContext = setContext(contextData.v);
-      result = renderToString(contextData.c);
-      restoreContext(prevContext);
-      return result;
-    } else {
-      return renderToString(op.d.children);
-    }
-  }
-  // TrackByKey Node
-  result = "";
-  const childrenNodes = (op.d as Key<any, Op>[]);
-  for (let i = 0; i < childrenNodes.length; ++i) {
-    result += renderToString(childrenNodes[i].v);
-  }
-  return result;
 }
 
 /**
@@ -137,12 +63,101 @@ function renderNode(op: OpNode): string {
 function _renderToString(op: Op): string {
   if (op !== null) {
     if (typeof op === "object") {
+      let result = "";
       if (op instanceof Array) {
-        return renderFragment(op);
+        for (let i = 0; i < op.length; ++i) {
+          result += renderToString(op[i]);
+        }
+        return result;
       }
-      return renderNode(op);
+
+      const flags = op.t.f;
+      if ((flags & NodeFlags.Element) !== 0) {
+        const data = op.d;
+        let openElement = `<${op.t.d}`;
+        let children = "";
+
+        const className = data.n;
+        if (className !== void 0 && className !== "") {
+          openElement += ` class="${className}"`;
+        }
+        let attrs = data.a;
+        if ((op.t.f & NodeFlags.ElementProto) !== 0 && (op.t.d as ElementProtoDescriptor).p.d.a !== void 0) {
+          attrs = { ...(op.t.d as ElementProtoDescriptor).p.d.a, ...attrs };
+        }
+        if (attrs !== void 0) {
+          for (const key in attrs) {
+            const value = attrs[key];
+            if (key === "style") {
+              for (const skey in value) {
+                emitStyle(`${skey}:${escapeAttributeValue(value[skey])}`);
+              }
+            } else {
+              if (typeof value === "object") {
+                (value as AttributeDirective<any>).s!(key, (value as AttributeDirective<any>).v);
+              } else if (typeof value !== "boolean") {
+                if (value !== null) {
+                  emitAttribute(`${key}="${escapeAttributeValue(value)}"`);
+                }
+              } else if (value === true) {
+                emitAttribute(key);
+              }
+            }
+          }
+          if (_styles !== "") {
+            emitAttribute(`style="${_styles}"`);
+            _styles = "";
+          }
+          openElement += _attributes;
+          children = _children;
+          _attributes = "";
+          _children = "";
+        }
+
+        if (children === "") {
+          children = renderToString((op as OpNode<ElementData>).d.c);
+        }
+        if ((flags & NodeFlags.NewlineEatingElement) !== 0) {
+          if (children.length > 0 && children.charCodeAt(0) === 10) { // "\n"
+            children = `\n${children}`;
+          }
+        }
+        return ((flags & (NodeFlags.VoidElement | NodeFlags.Svg)) !== 0 && children === "") ?
+          `${openElement} />` :
+          `${openElement}>${children}</${op.t.d}>`;
+      }
+      if ((flags & NodeFlags.Component) !== 0) {
+        if ((flags & NodeFlags.Stateful) !== 0) {
+          const stateNode = createStateNode(op);
+          stateNode.s = { r: null, s: null, u: null };
+          return renderToString((op.t.d as ComponentDescriptor).c(stateNode)(op.d));
+        } else {
+          return renderToString((op.t.d as StatelessComponentDescriptor).c(op.d));
+        }
+      }
+      if ((flags & (NodeFlags.Events | NodeFlags.Context)) !== 0) {
+        if ((flags & NodeFlags.Context) !== 0) {
+          const contextData = (op.d as ContextData);
+          const prevContext = setContext(contextData.v);
+          result = renderToString(contextData.c);
+          restoreContext(prevContext);
+          return result;
+        } else {
+          return renderToString(op.d.children);
+        }
+      }
+      // TrackByKey Node
+      const childrenNodes = (op.d as Key<any, Op>[]);
+      for (let i = 0; i < childrenNodes.length; ++i) {
+        result += renderToString(childrenNodes[i].v);
+      }
+      return result;
     }
-    return renderText(op);
+    if (typeof op === "string") {
+      return escapeText(op);
+    }
+    // number
+    return op.toString();
   }
   return "";
 }
