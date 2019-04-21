1a0a20773387200640213115b2502ece608a00b8
Boris Kaul <localvoid@gmail.com>
2019-04-21 13:56:52 +0700

    Refactor check element tests

--- packages/ivi/src/debug/__tests__/check_element.spec.ts
+++ packages/ivi/src/debug/__tests__/check_element.spec.ts
@@ -1,56 +1,59 @@
-import { checkElement } from "../element";
+import { useHTML, useSVG } from "ivi-jest";
+
+const h = useHTML();
+const s = useSVG();
+const _ = void 0;
 
 beforeEach(() => {
   console.warn = jest.fn();
 });
 
-describe(`HTML Attributes`, () => {
-  describe(`input element`, () => {
-    test(`assigning step attribute after value should print a warning`, () => {
-      checkElement("input", { value: "1", step: "10" }, false);
+describe("HTML Attributes", () => {
+  describe("input element", () => {
+    test("assigning step attribute after value should print a warning", () => {
+      h.input(_, { value: h.VALUE("1"), step: "10" });
       expect(console.warn).toHaveBeenCalled();
     });
 
-    test(`assigning min attribute after value should print a warning`, () => {
-      checkElement("input", { value: "1", min: "10" }, false);
+    test("assigning min attribute after value should print a warning", () => {
+      h.input(_, { value: h.VALUE("1"), min: "10" });
       expect(console.warn).toHaveBeenCalled();
     });
 
-    test(`assigning max attribute after value should print a warning`, () => {
-      checkElement("input", { value: "1", max: "10" }, false);
-      expect(console.warn).toHaveBeenCalled();
-    });
-  });
-
-});
-
-describe(`SVG Attributes`, () => {
-  describe(`deprecated attributes`, () => {
-    test(`svg viewport`, () => {
-      checkElement("svg", { viewport: "1" }, true);
-      expect(console.warn).toHaveBeenCalled();
-    });
-
-    test(`view viewTarget`, () => {
-      checkElement("view", { viewTarget: "1" }, true);
+    test("assigning max attribute after value should print a warning", () => {
+      h.input(_, { value: h.VALUE("1"), max: "10" });
       expect(console.warn).toHaveBeenCalled();
     });
   });
 });
 
-describe(`style`, () => {
-  test(`newline characters in styles`, () => {
-    checkElement("div", { style: { background: "#333\n" } }, false);
+describe("SVG Attributes", () => {
+  describe("deprecated attributes", () => {
+    test("svg viewport", () => {
+      s.svg(_, { viewport: "1" });
+      expect(console.warn).toHaveBeenCalled();
+    });
+
+    test("view viewTarget", () => {
+      s.view(_, { viewTarget: "1" });
+      expect(console.warn).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("style", () => {
+  test("newline characters in styles", () => {
+    h.div(_, { style: { background: "#333\n" } });
     expect(console.warn).toHaveBeenCalled();
   });
 
-  test(`semicolon characters in styles`, () => {
-    checkElement("div", { style: { background: "#333;" } }, false);
+  test("semicolon characters in styles", () => {
+    h.div(_, { style: { background: "#333;" } });
     expect(console.warn).toHaveBeenCalled();
   });
 
-  test(`NaN values in styles`, () => {
-    checkElement("div", { style: { background: NaN } }, false);
+  test("NaN values in styles", () => {
+    h.div(_, { style: { background: NaN as any } });
     expect(console.warn).toHaveBeenCalled();
   });
 });
