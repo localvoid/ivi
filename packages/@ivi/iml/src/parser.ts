c147259fda0bfaec5a6aecf411127e952440c2e4
Boris Kaul <localvoid@gmail.com>
2023-03-22 11:11:01 +0400

    Add `"string"` syntax in iml

--- packages/@ivi/iml/src/parser.ts
+++ packages/@ivi/iml/src/parser.ts
@@ -41,16 +41,14 @@ export class TemplateParser extends TemplateScanner {
     while (this.indent > indent && !this.isEnd()) {
       const c = this.peekCharCode();
       if (c !== -1) {
-        if (c === CharCode.SingleQuote || c === CharCode.Hash) {
-          const value = this.parseString(false);
-          if (value.length > (1 << 16)) {
-            throw new TemplateParserError("Text string is too long (>64k)", this.e, this.i - 1);
-            // Text nodes are splitted into two nodes when they exceed their length limit (64k).
-            // https://github.com/chromium/chromium/blob/91159249db3086f17b28b7a060f55ec0345c24c7/third_party/blink/renderer/core/dom/text.h#L42
-          }
+        if (
+          c === CharCode.SingleQuote ||
+          c === CharCode.DoubleQuote ||
+          c === CharCode.Hash
+        ) {
           children.push({
             type: INodeType.Text,
-            value,
+            value: this.parseTextString(),
           });
         } else {
           children.push(this.parseElement());
@@ -210,8 +208,18 @@ export class TemplateParser extends TemplateScanner {
   attributeProp(properties: IProperty[], key: string) {
     let value: string | boolean | number = true;
     if (this.charCode(CharCode.EqualsTo)) { // =
-      value = this.parseString(true);
-      if (value === "") {
+      const c = this.peekCharCode();
+      if (c !== -1) {
+        if (
+          c === CharCode.SingleQuote ||
+          c === CharCode.DoubleQuote ||
+          c === CharCode.Hash
+        ) {
+          value = this.parseAttributeString();
+        } else {
+          throw new TemplateParserError("Expected a string or an expression.", this.e, this.i);
+        }
+      } else {
         value = this.expr();
         if (value === -1) {
           throw new TemplateParserError("Expected a string or an expression.", this.e, this.i);
@@ -241,59 +249,146 @@ export class TemplateParser extends TemplateScanner {
     } as IProperty);
   }
 
-  parseString(isAttribute: boolean): string {
+  parseAttributeString(): string {
     const text = this.text;
     const textLength = text.length;
     let i = this.i;
-    if (i < textLength) {
-      let s = "";
-      let hashDelim = 0;
-      let c;
+    let s = "";
+    let hashDelim = 0;
+    let c;
 
-      while ((c = text.charCodeAt(i++)) === CharCode.Hash && i < textLength) {
-        hashDelim++;
+    while ((c = text.charCodeAt(i)) === CharCode.Hash) {
+      hashDelim++;
+      if (i === textLength) {
+        throw new TemplateParserError("Invalid attribute string.", this.e, i);
       }
-
-      if (c === CharCode.SingleQuote) {
-        let start = i;
-        outer: while (i < textLength) {
-          c = c = text.charCodeAt(i++);
-          if (c === CharCode.SingleQuote) {
-            const end = i - 1;
-            const j = i + hashDelim;
-            if (j > textLength) {
-              throw new TemplateParserError("Invalid string.", this.e, this.i);
-            }
-            while (i < j) {
-              if (text.charCodeAt(i) !== CharCode.Hash) {
-                continue outer;
-              }
-              i++;
-            }
-            this.i = i;
-            return s + text.substring(start, end);
-          }
-
-          let escaped: string;
-          if (c === CharCode.Ampersand) {
-            escaped = "&amp;"; // &
-          } else if (isAttribute === true) {
-            if (c !== CharCode.DoubleQuote) {
-              continue;
-            }
-            escaped = "&quot;";
-          } else if (c === CharCode.LessThan) { // StringContext.Text
-            escaped = "&lt;";
-          } else {
-            continue;
-          }
-
-          s += text.substring(start, i - 1) + escaped;
-          start = i;
-        }
-      }
+      i++;
     }
-    return "";
+
+    let delimCharCode: number;
+    if (c === CharCode.SingleQuote) {
+      delimCharCode = CharCode.SingleQuote;
+    } else if (c === CharCode.DoubleQuote) {
+      delimCharCode = CharCode.DoubleQuote;
+    } else {
+      throw new TemplateParserError("Expected ' or \" character.", this.e, i);
+    }
+
+    let start = ++i;
+    outer: while (i < textLength) {
+      c = text.charCodeAt(i++);
+      if (c === delimCharCode) {
+        const end = i - 1;
+        const j = i + hashDelim;
+        if (j > textLength) {
+          throw new TemplateParserError(
+            `Attribute string should be closed with ${hashDelim} '#' characters.`,
+            this.e,
+            i,
+          );
+        }
+        while (i < j) {
+          if (text.charCodeAt(i) !== CharCode.Hash) {
+            continue outer;
+          }
+          i++;
+        }
+        this.i = i;
+        return s + text.substring(start, end);
+      }
+
+      let escaped: string;
+      if (c === CharCode.Ampersand) {
+        escaped = "&amp;"; // &
+      } else if (c === CharCode.DoubleQuote) {
+        escaped = "&quot;";
+      } else {
+        continue;
+      }
+
+      s += text.substring(start, i - 1) + escaped;
+      start = i;
+    }
+
+    throw new TemplateParserError(
+      `Attribute string should be closed with a '${String.fromCharCode(delimCharCode)}' character`,
+      this.e,
+      i,
+    );
+  }
+
+  parseTextString(): string {
+    const text = this.text;
+    const textLength = text.length;
+    let i = this.i;
+    let s = "";
+    let hashDelim = 0;
+    let c;
+
+    while ((c = text.charCodeAt(i)) === CharCode.Hash) {
+      hashDelim++;
+      if (i === textLength) {
+        throw new TemplateParserError("Invalid text string.", this.e, i);
+      }
+      i++;
+    }
+
+    let delimCharCode: number;
+    if (c === CharCode.SingleQuote) {
+      delimCharCode = CharCode.SingleQuote;
+    } else if (c === CharCode.DoubleQuote) {
+      delimCharCode = CharCode.DoubleQuote;
+    } else {
+      throw new TemplateParserError("Expected ' or \" character.", this.e, i);
+    }
+
+    let start = ++i;
+    outer: while (i < textLength) {
+      c = text.charCodeAt(i++);
+      if (c === delimCharCode) {
+        const end = i - 1;
+        const j = i + hashDelim;
+        if (j > textLength) {
+          throw new TemplateParserError(
+            `Text string should be closed with ${hashDelim} '#' characters.`,
+            this.e,
+            i,
+          );
+        }
+        while (i < j) {
+          if (text.charCodeAt(i) !== CharCode.Hash) {
+            continue outer;
+          }
+          i++;
+        }
+        s += text.substring(start, end);
+        if (s.length > (1 << 16)) {
+          throw new TemplateParserError("Text string is too long (>64k)", this.e, i - 1);
+          // Text nodes are splitted into two nodes when they exceed their length limit (64k).
+          // https://github.com/chromium/chromium/blob/91159249db3086f17b28b7a060f55ec0345c24c7/third_party/blink/renderer/core/dom/text.h#L42
+        }
+        this.i = i;
+        return s;
+      }
+
+      let escaped: string;
+      if (c === CharCode.Ampersand) {
+        escaped = "&amp;"; // &
+      } else if (c === CharCode.LessThan) { // StringContext.Text
+        escaped = "&lt;";
+      } else {
+        continue;
+      }
+
+      s += text.substring(start, i - 1) + escaped;
+      start = i;
+    }
+
+    throw new TemplateParserError(
+      `Text string should be closed with a '${String.fromCharCode(delimCharCode)}' character`,
+      this.e,
+      i,
+    );
   }
 
   skipWhitespace(): boolean {
