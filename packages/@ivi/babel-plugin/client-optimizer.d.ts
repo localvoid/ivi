7d5d0d98ed624ace25f94287fbf67877aaaba054
Boris Kaul <localvoid@gmail.com>
2024-11-29 20:04:28 +0700

    Remove babel plugin

--- packages/@ivi/babel-plugin/client-optimizer.d.ts
+++
@@ -1,9 +0,0 @@
-import type * as babel from "@babel/core";
-
-export interface PluginConfig {
-  readonly dedupePropData?: false | "chunk" | "bundle";
-  readonly sharedPropData?: Map<string, number>;
-}
-
-declare const clientOptimizer: (config?: PluginConfig) => babel.PluginObj;
-export default clientOptimizer;
